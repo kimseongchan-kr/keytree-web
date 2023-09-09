424c0f5308396d033cc0df9fbfa60345fb52be3c
sumnii <song.sumin.kr@gmail.com>
2023-09-09 16:58:10 +0900

    feat: onSubmit의 타입을 위한 form field values 인터페이스 정의

--- src/components/ScheduleForm/index.tsx
+++ src/components/ScheduleForm/index.tsx
@@ -1,24 +1,34 @@
-"use client"
+import { useForm } from "react-hook-form";
 
-import {useForm} from "react-hook-form";
+interface ScheduleFormValues {
+  title: string;
+  place: string;
+  date: string;
+  time: string;
+  members: string;
+  notes: string;
+}
 
 export default function ScheduleForm() {
-    const {register, handleSubmit} = useForm();
+  const { register, handleSubmit } = useForm<ScheduleFormValues>();
 
-    function onSubmit(data) {
-        console.log(data);
-    }
+  function onSubmit(data: ScheduleFormValues) {
+    console.log(data);
+  }
 
-    return <div style={{display: "inline-block", borderRadius: "0.5rem", backgroundColor: "gray"}}>
-        <form style={{display: "flex", flexDirection: "column", alignItems: "center", gap: "2px"}}
-              onSubmit={handleSubmit(onSubmit)}>
-            <input {...register("title", {required: true})} placeholder={"Event Title"}/>
-            <input {...register("place")} placeholder={"Add Place"}/>
-            <input {...register("date", {required: true})} placeholder={"Add Date"}/>
-            <input {...register("time", {required: true})} placeholder={"Add Time"}/>
-            <input {...register("members")} placeholder={"Add Members"}/>
-            <input {...register("notes")} placeholder={"Add Notes"}/>
-            <button>add</button>
-        </form>
+  return (
+    <div style={{ display: "inline-block", borderRadius: "0.5rem", border: "1px solid gray" }}>
+      <form
+        style={{ display: "flex", flexDirection: "column", alignItems: "center", gap: "2px" }}
+        onSubmit={handleSubmit(onSubmit)}>
+        <input {...register("title", { required: true })} placeholder={"Event Title"} />
+        <input {...register("place")} placeholder={"Add Place"} />
+        <input {...register("date", { required: true })} placeholder={"Add Date"} />
+        <input {...register("time", { required: true })} placeholder={"Add Time"} />
+        <input {...register("members")} placeholder={"Add Members"} />
+        <input {...register("notes")} placeholder={"Add Notes"} />
+        <button>add</button>
+      </form>
     </div>
+  );
 }
